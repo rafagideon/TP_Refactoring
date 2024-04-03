aa4b830a7bbb12b47e0cbfcc1d9c9a9a314f4078
RFG <RFG@RFG-PC>
2024-04-03 20:38:55 +0200

    13)

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -12,4 +12,10 @@ public class Femme extends Humain {
 	public Femme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 	}
+	
+	public static void main() {
+		Homme jamesBond = new Homme("James", "Bond", 35);
+		Femme kantoSoa = new Femme("Kanto", "Soa", 32);
+		jamesBond.ami(kantoSoa);
+	}
 }
