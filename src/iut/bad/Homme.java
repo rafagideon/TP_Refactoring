2be8e4fade8259732379c87f18d9b610bcefe902
RFG <RFG@RFG-PC>
2024-04-03 19:44:30 +0200

    03)

---
+++ src/iut/bad/Homme.java
@@ -0,0 +1,26 @@
+package iut.bad;
+
+public class Homme {
+	private String nom;
+	private String prenom;
+	private int age;
+	
+	public Homme() {
+		super();
+	}
+	
+	public Homme(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public Homme(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	
+	
+}
